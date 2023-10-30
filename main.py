37d0c1dae24e852619a33b77f86a01ead8ecc506
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-10-30 14:25:39 +0400

    control movement from keyboard

--- main.py
+++ main.py
@@ -5,7 +5,21 @@ screen = Screen()
 
 def move_forwards():
     tim.forward(10)
+def move_backwards():
+    tim.backward(10)
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+
 
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(move_forwards, "w")
+screen.onkey(move_backwards, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+
 screen.exitonclick()
