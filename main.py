9d971e4bbb66f3260667175346215a78463c6e00
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-11-01 12:36:50 +0400

    placement of turtles according to index

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from turtle import Turtle, Screen
 screen = Screen()
 screen.setup(500, 400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
-print(user_bet)
+colors = ["red", "orange", "blue", "green", "yellow", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
 
-tim = Turtle(shape="turtle")
-tim.speed(1)
-tim.penup()
-tim.goto(x=-220, y=-100)
+for turtle_index in range(0, 6):
+    tim = Turtle(shape="turtle")
+    tim.speed(1)
+    tim.penup()
+    tim.goto(x=-220, y=y_positions[turtle_index])
 
 
 screen.exitonclick()
